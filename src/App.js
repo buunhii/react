2d955ca0a354f61d73eedb97f24022bb485bc8e1
buunhii <buunhiii@gmail.com>
2021-01-20 15:33:09 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LayoutBootstrap from "./LayoutBootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutBootstrap />
+    </div>
+  );
+}
+
+export default App;
